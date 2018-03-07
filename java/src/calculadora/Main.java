e5d4d3ed81d9f843c63ba57d0916af4d7d3116d7
s0orana <guilherme_leme@yahoo.com.br>
2018-03-06 21:33:57 -0300

    Parte 1 OK
    OBS: Verificar Bugs na função converte()

---
+++ java/src/calculadora/Main.java
@@ -0,0 +1,27 @@
+package calculadora;
+
+import java.util.Scanner;
+
+import validadorExpressao.Validar;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		start();
+	}
+
+	public static void start(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite sua expressao:");
+		Validar validador = new Validar(sc.nextLine());
+		try{
+			boolean ok = validador.valida();
+			System.out.println(ok);
+			start();
+		}catch(Exception e){
+			System.err.println(e);
+			start();
+		}
+	}
+}
---
+++ java/src/validadorExpressao/Validar.java
@@ -0,0 +1,157 @@
+package validadorExpressao;
+
+import java.util.StringTokenizer;
+
+public class Validar {
+	private String str;
+	public Validar(String string){
+		this.str = string;
+	}
+	
+	public boolean valida() throws Exception{
+		tiraEspaco();
+		converte();
+		caracterInvalido();
+		return expressao();
+	}
+	public void tiraEspaco(){
+		StringBuffer newstr = new StringBuffer();
+		StringTokenizer quebrador = new StringTokenizer (this.str, " ");
+		while(quebrador.hasMoreTokens() == true){
+			newstr.append(quebrador.nextToken());
+		}
+		str = newstr.toString();
+	}
+	public void converte(){
+		str = str.replaceAll("<->", "<");
+		str = str.replaceAll("->", "-");
+		str = str.replaceAll("t", "T");
+		str = str.replaceAll("f", "F");
+		str = str.replaceAll("V", "v");
+		str = str.replaceAll("∨", "v");
+		str = str.replaceAll("∧", "^");
+		str = str.replaceAll("ˆ", "^");
+		str = str.replaceAll("∼", "~");
+		str = str.replaceAll("˜", "~");
+	}
+	public void caracterInvalido() throws Exception{
+		String palavra[] = str.split("");
+		for (int x = 0; x < palavra.length; x++){
+			if(!palavra[x].contains("(") && !palavra[x].contains("~") && !palavra[x].contains("^") && !palavra[x].contains("v") && 
+					!palavra[x].contains("-") && !palavra[x].contains("<") && !palavra[x].contains(")") && !palavra[x].contains("T") && 
+					!palavra[x].contains("F")){
+				throw new Exception("Expressao com caracter invalido");
+			}
+		}
+	}
+	public boolean expressao(){
+		int parenteses = 0, operador = 0, negacao = 0, variavel = 0; //para retornar true, todos tem que estar 0
+		StringTokenizer quebrador = new StringTokenizer (this.str, "~^∨-<", true);
+		while(quebrador.hasMoreTokens() == true){
+			String palavraQuebrada[] = quebrador.nextToken().split("");
+			for(int x = 0; x < palavraQuebrada.length; x++){
+				//()~^v-<
+				switch (palavraQuebrada[x]){
+				case "(":
+					parenteses ++;
+					break;
+				case ")":
+					parenteses --;
+					break;
+				case "~":
+					negacao ++;
+					break;
+				case "^":
+					if(variavel > 0)
+						variavel --;
+					else if(variavel == 0)
+						return false;
+					if(negacao > 0)
+						return false;
+					if(operador > 0)
+						return false;
+					operador ++;
+					break;
+				case "v":
+					if(variavel > 0)
+						variavel --;
+					else if(variavel == 0)
+						return false;
+					if(negacao > 0)
+						return false;
+					if(operador > 0)
+						return false;
+					operador ++;
+					break;
+				case "-":
+					if(variavel > 0)
+						variavel --;
+					else if(variavel == 0)
+						return false;
+					if(negacao > 0)
+						return false;
+					if(operador > 0)
+						return false;
+					operador ++;
+					break;
+				case "<":
+					if(variavel > 0)
+						variavel --;
+					else if(variavel == 0)
+						return false;
+					if(negacao > 0)
+						return false;
+					if(operador > 0)
+						return false;
+					operador ++;
+					break;
+				case "T":
+					if(negacao > 0)
+						negacao --;
+					if(operador > 0)
+						operador --;
+					if(variavel > 0)
+						return false;
+					variavel ++;
+					break;
+				case "F":
+					if(negacao > 0)
+						negacao --;
+					if(operador > 0)
+						operador --;
+					if(variavel > 0)
+						return false;
+					variavel ++;
+					break;
+				default:
+					System.err.println("Caracter invalido - " + palavraQuebrada[x]);
+					return false;
+				}
+				if(quebrador.hasMoreTokens() == false && x == (palavraQuebrada.length - 1)){
+					//verifica ultima palavra
+					switch (palavraQuebrada[x]){
+					case ")":
+						break;
+					case "T":
+						break;
+					case "F":
+						break;
+					default :
+						return false;
+					}
+					//Variavel tem que ser 1 para ser valida, isso significa que nenhum operador ou negacao esta terminando a frase.
+					if(variavel != 1){
+						return false;
+					}else{
+						variavel --;
+					}
+				}
+			}
+		}
+		if((parenteses + operador + negacao + variavel) == 0){
+			return true;
+		}else{			
+			return false;
+		}
+	}
+}
